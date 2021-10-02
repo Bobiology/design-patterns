e59597ee89bf511b991d559d855209ead52f1e91
Sakawa Bob <bob.sakawa@gmail.com>
2021-10-02 16:21:54 +0300

    Update Main.java

--- src/Notification/Main.java
+++ src/Notification/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Notification101 notification101 = new Notification101();
-		notification101.sendEmail("bob.sakawa@gmail.com");
+		notification101.sendEmail("XXXXXXXXXX");
 		System.out.println();
-		notification101.sendSMS("0704419289");
+		notification101.sendSMS("XXXXXXXXX");
 	}
 }
